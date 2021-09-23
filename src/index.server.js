1f885f6b690536afb956e4da3a7f59c0153172d4
spartaabhishek <abhishek.soni2019@vitstudent.ac.in>
2021-09-23 15:09:22 +0530

    user route

---
+++ src/index.server.js
@@ -0,0 +1,24 @@
+const express=require("express");
+const env=require('dotenv')
+const mongoose = require('mongoose')
+const app=express();
+const userRoutes=require('./routes/user')
+//environment variable 
+env.config();
+//mongodb connection
+//mongodb+srv://abhishek:<password>@cluster0.grgsb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.grgsb.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    }
+).then(()=>{
+    console.log('database created')
+})
+app.use(express.json())
+
+app.use('/api',userRoutes)
+
+app.listen(process.env.PORT,()=>{
+console.log(`server is running on ${process.env.PORT}`)
+})
