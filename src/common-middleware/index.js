870a1ff41868b445e63cb28c861e2614900b8efc
spartaabhishek <abhishek.soni2019@vitstudent.ac.in>
2021-09-29 22:31:58 +0530

    categories added

--- src/common-middleware/index.js
+++ src/common-middleware/index.js
@@ -1,8 +1,21 @@
+const jwt = require("jsonwebtoken");
 exports.requireSignin = (req, res, next) => {
+  if (req.headers.authorization) {
     const token = req.headers.authorization.split(" ")[1];
     const user = jwt.verify(token, process.env.JWT_SECRET);
     req.user = user;
-    next();
-    console.log(token);
-  };
-  
+  } else return res.status(400).json({ message: "invalid authorization " });
+  next();
+};
+exports.userMiddleware = (req, res, next) => {
+  if (req.user.role != "user") {
+    return res.status(400).json({ message: "user access denoed" });
+  }
+  next();
+};
+exports.adminMiddleware = (req, res, next) => {
+  if (req.user.role != "admin") {
+    return res.status(400).json({ message: "admin access denoed" });
+  }
+  next();
+};
