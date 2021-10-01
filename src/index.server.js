6ceb46d38db648c666f320aef7669e966dd76e38
spartaabhishek <abhishek.soni2019@vitstudent.ac.in>
2021-10-01 23:47:09 +0530

    cart added

--- src/index.server.js
+++ src/index.server.js
@@ -6,6 +6,8 @@ const authRoutes = require("./routes/auth");
 const adminRoutes = require("./routes/admin/auth");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
+const cartRoutes = require("./routes/cart");
+
 //environment variable
 env.config();
 //mongodb connection
@@ -23,6 +25,7 @@ app.use("/api", authRoutes);
 app.use("/api", adminRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
+app.use("/api", cartRoutes);
 app.listen(process.env.PORT, () => {
   console.log(`server is running on ${process.env.PORT}`);
 });
