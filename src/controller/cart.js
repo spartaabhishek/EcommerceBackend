c53049fe56e478c4a83511709cde6627ca926a03
spartaabhishek <abhishek.soni2019@vitstudent.ac.in>
2021-10-02 21:40:46 +0530

    cart api added

--- src/controller/cart.js
+++ src/controller/cart.js
@@ -4,37 +4,44 @@ exports.addToCart = (req, res) => {
     if (error) return res.status(400).json({ error });
     if (cart) {
       //if cart laready xist then update cart by quantity
-      const product=req.body.cartItems.product
-      const item = cart.cartItems.find(
-        (c) => c.product == product
-      );
+      const product = req.body.cartItems.product;
+      const item = cart.cartItems.find((c) => c.product == product);
+      let condition, action;
       if (item) {
-        Cart.findOneAndUpdate(
-          { user: req.user._id ,"cartItems.product":product},
-          {
-            "$set": {
-              "cartItems": {...req.body.cartItems,
-            quantity:item.quantity+req.body.cartItems.quantity},
+        condition = { user: req.user._id, "cartItems.product": product };
+        action = {
+          $set: {
+            "cartItems.$": {
+              ...req.body.cartItems,
+              quantity: item.quantity + req.body.cartItems.quantity,
             },
-          }
-        ).exec((error, _cart) => {
-          if (error) return res.status(400).json({ error });
-          if (_cart) {
-            return res.status(201).json({ _cart });
-          }
-        });
-      } 
+          },
+        };
+      } else {
+        condition = { user: req.user._id };
+        action = {
+          $push: {
+            cartItems: req.body.cartItems,
+          },
+        };
+      }
+      Cart.findOneAndUpdate(condition, action).exec((error, _cart) => {
+        if (error) return res.status(400).json({ error });
+        if (_cart) {
+          return res.status(201).json({ _cart });
+        }
+      });
     } else {
-        const cart = new Cart({
-            user: req.user._id,
-            cartItems: [req.body.cartItems],
-          });
-          cart.save((error, cart) => {
-            if (error) return res.status(400).json({ error });
-            if (cart) {
-              return res.status(201).json({ cart });
-            }
-          });
+      const cart = new Cart({
+        user: req.user._id,
+        cartItems: [req.body.cartItems],
+      });
+      cart.save((error, cart) => {
+        if (error) return res.status(400).json({ error });
+        if (cart) {
+          return res.status(201).json({ cart });
+        }
+      });
     }
   });
 };
