03ccaac279e5c31e3911fe123094ffa0a5337766
spartaabhishek <abhishek.soni2019@vitstudent.ac.in>
2021-10-07 23:45:42 +0530

    code ni chala

--- src/index.server.js
+++ src/index.server.js
@@ -7,20 +7,22 @@ const adminRoutes = require("./routes/admin/auth");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
 const cartRoutes = require("./routes/cart");
-const path =require('path')
+const path = require('path')
+const cors=require('cors')
 //environment variable
 env.config();
 //mongodb connection
 //mongodb+srv://abhishek:<password>@cluster0.grgsb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.grgsb.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`
+    `mongodb://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0-shard-00-00.grgsb.mongodb.net:27017,cluster0-shard-00-01.grgsb.mongodb.net:27017,cluster0-shard-00-02.grgsb.mongodb.net:27017/${process.env.MONGO_DB_DATABASE}?ssl=true&replicaSet=atlas-1vr52k-shard-0&authSource=admin&retryWrites=true&w=majority`
   )
   .then(() => {
     console.log("database created");
   });
 app.use(express.json());
-app.use('/public',express.static(path.join(__dirname,'uploads')))
+app.use(cors())
+app.use('/public', express.static(path.join(__dirname, 'uploads')))
 app.use("/api", authRoutes);
 app.use("/api", adminRoutes);
 app.use("/api", categoryRoutes);
